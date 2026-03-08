70888e753815513d6b6a13f3365f4021b1ba94ea
sjohns <integratedinternetapps2@gmail.com>
2026-03-08 19:41:35 -0400

    fix: add DigitalOcean URL to CORS allowed origins

--- H2O/api/app.py
+++ H2O/api/app.py
@@ -28,7 +28,7 @@ reload_runtime_snapshot()
 app = FastAPI(title="H2O API", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8001", "http://localhost:8001"],
+    allow_origins=["http://127.0.0.1:8001", "http://localhost:8001", "https://h2o-api-97ivf.ondigitalocean.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
